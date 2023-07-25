e483db853acf13e1ec0de48ab9475452fde50eb0
Anthony A. Garcia <angarce01@gmail.com>
2023-07-24 18:44:46 -0700

    WIP - multi step registration page

--- flask_app/controllers/users.py
+++ flask_app/controllers/users.py
@@ -14,6 +14,48 @@ def submit_registration():
     user.User.create_user(request.form)
     return redirect('/account_creation/success')
 
+#-WORK IN PROGRESS- STEP BY STEP REGISTRATION FORM
+# @app.route('/register', methods=['GET', 'POST'])
+# def register():
+#     if request.method == 'POST':
+#         # Step 1: Process the User form data
+#         if 'step' not in session:
+#             session['step'] = 1
+#             session['user_data'] = {
+#                 'username': request.form['username'],
+#                 'email': request.form['email'],
+#                 # Add other user-related fields
+#             }
+#             return redirect(url_for('register'))
+
+#         # Step 2: Process the Dog form data
+#         elif session['step'] == 1:
+#             session['step'] = 2
+#             session['dog_data'] = {
+#                 'dog_name': request.form['dog_name'],
+#                 'dog_breed': request.form['dog_breed'],
+#                 # Add other dog-related fields
+#             }
+#             return redirect(url_for('register'))
+
+#         # Step 3: Process the final step with password and save to the database
+#         elif session['step'] == 2:
+#             session.pop('step', None)  # Remove the step key from the session
+#             # Combine user and dog data
+#             user_data = session['user_data']
+#             dog_data = session['dog_data']
+
+#             # Additional processing for password (e.g., hashing)
+#             user_data['password'] = request.form['password']
+
+#             # Save user_data and dog_data to the database
+#             # Implement the database storage logic here
+
+#             # Redirect to a thank you or confirmation page
+#             return redirect(url_for('confirmation'))
+
+#     return render_template('register.html')
+
 @app.route('/register/2')
 def show_registration_2():
     return render_template('register_two.html')
